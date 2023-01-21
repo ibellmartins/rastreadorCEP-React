4c38f6974f73e86cc93a01e361efd4b36b557e8b
Isabella <bellasofiamartins@gmail.com>
2023-01-21 01:43:38 -0300

    cep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import{useState} from 'react';
+import{FiSearch} from 'react-icons/fi';
+import './style.css';
+import api from './services/api';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [cep, setCep] = useState('');
+
+  async function pesqSearch(){
+
+    if (input==''){
+      alert('Preencha o campo de acordo com a instrução!')
+      return;
+    }
+
+    try{
+      const response = await api.get(`${input}/json`);
+      setCep(response.data); //passa a informacao pro useState
+      setInput(""); //limpa o campo do input
+    }
+    catch{
+      alert('Erro ao buscar seu CEP.');
+      setInput(""); //após mostrar erro e o usuário clicar em OK, o campo vai ser limpo
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Busca CEP</h1>
+
+      <div className="containerInput">
+      <input type="text" 
+      placeholder="Digite o seu CEP aqui"
+      value={input}
+      onChange={(ev) => setInput(ev.target.value) } 
+      
+      />
+      <button className="btnSearch" onClick={pesqSearch}>
+        <FiSearch size={20} color="black"/>
+      </button>
+      </div>
+
+      {Object.keys(cep).length > 0 &&(
+            <main className='main'>
+            <h2>CEP: {cep.cep} </h2>
+            <span>Rua: {cep.logradouro} </span>
+            <span>Bairro: {cep.bairro}</span>
+            <span>Cidade: {cep.localidade} - {cep.uf}</span>
+            </main>  
+      )}
     </div>
   );
 }
